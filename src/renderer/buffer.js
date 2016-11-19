7539189820b74e0f1d1e923c37fc60b2f4313b0f
Duknam Yoo <yoo2001818@gmail.com>
2016-11-19 21:13:42 +0900

    Implement sub-updating buffer. v1.0.13

--- src/renderer/buffer.js
+++ src/renderer/buffer.js
@@ -15,6 +15,15 @@ export default class Buffer {
     }
     this.upload();
   }
+  updateSub(data, offset) {
+    if (this.vbo == null) this.upload();
+    // Update subdata...
+    this.data.set(data, offset);
+    const gl = this.renderer.gl;
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
+    gl.bufferSubData(gl.ARRAY_BUFFER, offset * 4, data);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  }
   upload() {
     const gl = this.renderer.gl;
     if (this.vbo == null) this.vbo = gl.createBuffer();
