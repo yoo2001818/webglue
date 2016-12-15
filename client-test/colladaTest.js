fb9d9afe80d573484e0a8ce1d7d5f3be190eda1b
Duknam Yoo <yoo2001818@gmail.com>
2016-12-15 16:37:10 +0900

    Transfer collada loader to separate repository

--- client-test/colladaTest.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-import loadCollada from '../src/loader/collada';
-
-loadCollada(fs.readFileSync('./geom/cat.dae', 'utf-8'));
--- client-test/scene/collada.js
+++
@@ -1,6 +0,0 @@
-import loadCollada from 'webglue/loader/collada';
-
-export default function collada() {
-  let data = loadCollada(require('../geom/cat.dae'));
-  return () => {};
-}
